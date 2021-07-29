9e18ed14583a2695960d597a56b1260b7d1fd537
JinYeongKim <handayoung11@gmail.com>
2021-07-30 01:27:00 +0900

    해커뉴스에서 데이터를 얻어와 렌더링하는 로직 작성

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
-var colors = require('colors');
-console.log('Hello World'.yellow);
+const ajax = new XMLHttpRequest();
+const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
+
+ajax.open('GET', NEWS_URL, false);
+ajax.send();
+
+const newsFeed = JSON.parse(ajax.response);
+const ul = document.createElement('ul');let li = '';
+
+for(let i = 0; i < 10; i++) {
+    const li = document.createElement('li');
+    li.innerText = newsFeed[i].title;
+    ul.appendChild(li);
+}
+
+document.getElementById('root').appendChild(ul);
