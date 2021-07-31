a5727cc3c444bdf95ee8e4302c08a9383b48cdde
JinYeongKim <handayoung11@gmail.com>
2021-07-31 16:33:29 +0900

    제목 클릭 시 하단에 제목이 나타나도록 로직 작성

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+const container = document.getElementById('root');
 const ajax = new XMLHttpRequest();
+const content = document.createElement('div');
+const title = document.createElement('h1');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
+const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 
 ajax.open('GET', NEWS_URL, false);
 ajax.send();
@@ -7,10 +11,27 @@ ajax.send();
 const newsFeed = JSON.parse(ajax.response);
 const ul = document.createElement('ul');let li = '';
 
+window.addEventListener('hashchange', function() {
+    const id = location.hash.substr(1);
+    ajax.open('GET', CONTENT_URL.replace('@id', id), false);
+    ajax.send();
+
+    const newsContent = JSON.parse(ajax.response);
+    title.innerText = newsContent.title;
+});
+
 for(let i = 0; i < 10; i++) {
     const li = document.createElement('li');
-    li.innerText = newsFeed[i].title;
+    const a = document.createElement('a');
+    a.innerText = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
+    a.href = `#${newsFeed[i].id}`;
+    
+    a.addEventListener('click', function() {});
+
+    li.appendChild(a);
     ul.appendChild(li);
 }
 
-document.getElementById('root').appendChild(ul);
+content.appendChild(title);
+container.appendChild(ul);
+container.appendChild(content);
