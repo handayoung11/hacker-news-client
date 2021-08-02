c40afbf989aa44a729e3535841fa9003a451eb91
JinYeongKim <handayoung11@gmail.com>
2021-08-02 19:11:31 +0900

    글 상세 페이지의 UI 구현: 문자열 -> template 방식으로 수정

--- app.js
+++ app.js
@@ -48,16 +48,35 @@ function newsFeed() {
 
 function newsDetail() {
     const id = location.hash.substr(7);
-    
+
     const newsContent = getData(CONTENT_URL.replace('@id', id));
-    const title = document.createElement('h1');
-
-    container.innerHTML = `
-        <h1>${newsContent.title}</h1>
-
-        <div>
-            <a href='#/page/${store.currentPage}'>목록으로</a>
+    let template = `
+    <div class="bg-gray-600 min-h-screen pb-8">
+      <div class="bg-white text-xl">
+        <div class="mx-auto px-4">
+          <div class="flex justify-between items-center py-6">
+            <div class="flex justify-start">
+              <h1 class="font-extrabold">Hacker News</h1>
+            </div>
+            <div class="items-center justify-end">
+              <a href="#/page/${store.currentPage}" class="text-gray-500">
+                <i class="fa fa-times"></i>
+              </a>
+            </div>
+          </div>
         </div>
+      </div>
+
+      <div class="h-full border rounded-xl bg-white m-6 p-4 ">
+        <h2>${newsContent.title}</h2>
+        <div class="text-gray-400 h-20">
+          ${newsContent.content}
+        </div>
+
+        {{__comments__}}
+
+      </div>
+    </div>
     `;
 }
 
