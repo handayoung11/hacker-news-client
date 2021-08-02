6a02e67a74c84078e3001ea673b5422fa531c908
JinYeongKim <handayoung11@gmail.com>
2021-08-03 02:30:54 +0900

    함수 타입 명시 및 makeComment 함수 선언 위치 변경

--- app.ts
+++ app.ts
@@ -38,18 +38,18 @@ const store: Store = {
   feeds: [],
 };
 
-function getData(url) {
+function getData<AjaxResponse>(url: string): AjaxResponse {
   ajax.open('GET', url, false);
   ajax.send();
   return JSON.parse(ajax.response);
 }
 
-function updateView(html) {
+function updateView(html: string): void {
   if (container) container.innerHTML = html;
   else console.error('최상위 컨테이너가 없어 UI를 진행하지 못합니다.');
 }
 
-function newsFeed() {
+function newsFeed(): void {
   let newsFeed: NewsFeed[] = store.feeds;
   const newsList = [];
   let template = `
@@ -78,7 +78,7 @@ function newsFeed() {
     `;
 
   if (newsFeed.length == 0) {
-    newsFeed = store.feeds = getData(NEWS_URL);
+    newsFeed = store.feeds = getData<NewsFeed[]>(NEWS_URL);
   }
 
   for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
@@ -110,10 +110,10 @@ function newsFeed() {
   updateView(template);
 }
 
-function newsDetail() {
+function newsDetail(): void {
   const id = location.hash.substr(7);
 
-  const newsContent = getData(CONTENT_URL.replace('@id', id));
+  const newsContent = getData<NewsDetail>(CONTENT_URL.replace('@id', id));
   let template = `
     <div class="bg-gray-600 min-h-screen pb-8">
       <div class="bg-white text-xl">
@@ -150,29 +150,31 @@ function newsDetail() {
     }
   }
 
-  function makeComment(comments, called = 0) {
-    const commentString = [];
-
-    for (let i = 0; i < comments.length; i++) {
-      commentString.push(`
-            <div style="padding-left: ${called * 40}px;" class="mt-4">
-              <div class="text-gray-400">
-                <i class="fa fa-sort-up mr-2"></i>
-                <strong>${comments[i].user}</strong> ${comments[i].time_ago}
-              </div>
-              <p class="text-gray-700">${comments[i].content}</p>
-            </div>      
-          `);
-      if (comments[i].comments.length > 0)
-        commentString.push(makeComment(comments[i].comments, called + 1));
-    }
-    return commentString.join('');
-  }
-
   updateView(template.replace('{{__comments__}}', makeComment(newsContent.comments)));
 }
 
-function router() {
+function makeComment(comments: NewsComment[]): string {
+  const commentString = [];
+
+  for (let i = 0; i < comments.length; i++) {
+    const comment: NewsComment = comments[i];
+
+    commentString.push(`
+          <div style="padding-left: ${comment.level * 40}px;" class="mt-4">
+            <div class="text-gray-400">
+              <i class="fa fa-sort-up mr-2"></i>
+              <strong>${comment.user}</strong> ${comment.time_ago}
+            </div>
+            <p class="text-gray-700">${comment.content}</p>
+          </div>      
+        `);
+    if (comment.comments.length > 0)
+      commentString.push(makeComment(comment.comments));
+  }
+  return commentString.join('');
+}
+
+function router(): void {
   const routePath = location.hash;
 
   if (routePath == '') {
